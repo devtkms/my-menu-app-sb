0ffc318aae6cc119d0f058a4c342db0aba189bcf
Takumi Suzuki <devtkmszk824@gmail.com>
2024-06-18 22:06:18 +0900

    Add main application and test files
    - Add MyMenuAppSbApplication.kt as the main entry point
    - Add MyMenuAppSbApplicationTests.kt for application tests

---
+++ src/main/kotlin/com/devtkms/mymenuappsb/MyMenuAppSbApplication.kt
@@ -0,0 +1,11 @@
+package com.devtkms.mymenuappsb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyMenuAppSbApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyMenuAppSbApplication>(*args)
+}
